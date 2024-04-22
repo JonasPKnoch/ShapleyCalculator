9188ede37cb0e7f65ee3e2e58e5b579a6b73c8c9
unknown <jonas.p.knochelmann@gmail.com>
2024-04-21 19:39:46 -0600

    working calculator

--- shapley.py
+++ shapley.py
@@ -12,9 +12,9 @@ def powerSet(set):
     
     return pow
 
-def powerSetContaining(set, i):
-    exclude = set[i]
-    remaining = set[:i] + set[i + 1:]
+def powerSetContaining(s, i):
+    exclude = s[i]
+    remaining = s[:i] + s[i + 1:]
     pow = powerSet(remaining)
     for p in pow:
         p.append(exclude)
@@ -22,12 +22,25 @@ def powerSetContaining(set, i):
     return pow
 
 
-def shapley(n, v):
+def shapley(n, i, v):
     players = list(range(n))
     sum = 0
     nFac = math.factorial(n)
-    for i in range(len(set)):
-        s = powerSetContaining(set, i)
+    pi = powerSetContaining(players, i)
+    for s in pi:
         sSize = len(s)
         sumEl = v(s) - v(s[:-1])
         sumEl *= math.factorial(sSize - 1)*math.factorial(n - sSize)/nFac
+        sum += sumEl
+    
+    return sum
+
+def v(s):
+    if len(s) < 9:
+        return 0
+    for i in range(5):
+        if i not in s:
+            return 0
+    return 1
+
+print(shapley(15, 8, v))
