575b0aa145d43f6e013fdc3234cb1205a8b1412d
unknown <jonas.p.knochelmann@gmail.com>
2024-04-21 20:14:50 -0600

    variable

--- shapley.py
+++ shapley.py
@@ -35,6 +35,12 @@ def shapley(n, i, v):
     
     return sum
 
+numPermMembers = 5
+numNPermMembers = 10
+permVetoCount = 1
+nPermVetoCount = 99999
+winVotes = 9
+
 def v(s):
     if len(s) < 9:
         return 0
@@ -43,4 +49,36 @@ def v(s):
             return 0
     return 1
 
-print(shapley(15, 8, v))
+def vModified(s):
+    permVotes = 0
+    for i in range(0, numPermMembers):
+        if i in s:
+            permVotes += 1
+    permVetos = numPermMembers - permVotes
+
+    nPermVotes = 0
+    for i in range(numPermMembers, numPermMembers + numNPermMembers):
+        if i in s:
+            nPermVotes += 1
+    nPermVetos = numPermMembers - nPermVotes
+
+    if permVetos >= permVetoCount:
+        return 0
+        
+    if nPermVetos >= nPermVetoCount:
+        return 0
+        
+    if permVotes + nPermVotes < winVotes:
+        return 0
+    
+    return 1
+
+print(shapley(numPermMembers + numNPermMembers, 0, vModified) * 100)
+print(shapley(numPermMembers + numNPermMembers, 5, vModified ) * 100)
+
+for s in powerSet(list(range(15))):
+    break
+    v1 = v(s)
+    v2 = vModified(s)
+    if v1 != v2:
+        print(str(s) + ": " + str(v1) + " " + str(v2))
